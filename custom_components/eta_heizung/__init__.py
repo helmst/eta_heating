7faaf4de7ec4b9c41d7016a33b57e40d3a4e8cdb
Carsten Helmstädter <carsten.helmstaedter@ionos.com>
2025-08-15 17:51:00 +0200

    Some adjustments

--- custom_components/eta_heizung/__init__.py
+++ custom_components/eta_heizung/__init__.py
@@ -1,5 +1,26 @@
 """ETA Heizung Home Assistant Integration."""
 import logging
+from .const import DOMAIN, CONF_HOST, CONF_PORT
+from .eta_api import EtaApi
 
-DOMAIN = "eta_heizung"
 _LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, config_entry):
+    host = config_entry.data[CONF_HOST]
+    port = config_entry.data.get(CONF_PORT, 8080)
+    api = EtaApi(host, port)
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = {
+        "api": api,
+        "sensors": config_entry.options.get("sensors", []),
+        "switches": config_entry.options.get("switches", []),
+        "numbers": config_entry.options.get("numbers", []),
+    }
+    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    await hass.config_entries.async_forward_entry_setup(config_entry, "switch")
+    await hass.config_entries.async_forward_entry_setup(config_entry, "number")
+    return True
+
+async def async_setup(hass, config):
+    """Support YAML setup (falls gewünscht)."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
